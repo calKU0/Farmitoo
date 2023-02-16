e2a52eda78f58ecc3b41cc424e00ccf5e528e938
Krzysztof Kurowski <228528@student.uek.krakow.pl>
2023-02-16 08:51:59 +0100

    Changed the connection from ftp to sftp

--- StanyMagazynoweFarmitoo/StanyMagazynoweFarmitoo/Program.cs
+++ StanyMagazynoweFarmitoo/StanyMagazynoweFarmitoo/Program.cs
@@ -2,7 +2,8 @@
 using System.Data;
 using System.Data.SqlClient;
 using System.IO;
-using System.Net;
+using Renci.SshNet;
+
 
 
 namespace StanyMagazynoweFarmitoo
@@ -12,7 +13,7 @@ namespace StanyMagazynoweFarmitoo
         static void Main(string[] args)
         {
             // Łączenie się z bazą
-            string connectionString = "user id=Gaska;password=xxxx;Data Source=xxxx;Trusted_Connection=no;database=xxxx;connection timeout=5;";
+            string connectionString = "user id = xxxx; password = xxxx; Data Source = xxxx; Trusted_Connection = no; database = xxxx; connection timeout=5;";
 
             // Zapytanie
             string query = "Select distinct TOP 100 Twr_Ean as [reference], case when(isnull((select sum(TwZ_IlSpr) from cdn.TwrZasoby with(nolock) join cdn.TwrKarty us with(nolock) on Twr_GIDNumer = TwZ_TwrNumer where TwZ_MagNumer = 1 and us.Twr_Kod = ss.twr_kod), 0)- isnull((select sum(Rez_Ilosc) from cdn.Rezerwacje with(nolock) join cdn.TwrKarty us with(nolock) on Twr_GIDNumer = Rez_TwrNumer where Rez_MagNumer = 1 and us.twr_kod = ss.twr_kod and Rez_Aktywna = 1 and Rez_Typ = 1 and Rez_DataWaznosci > DATEDIFF(DD, '18001228', GETDATE())),0)) < 0 then 0 else cast(isnull((select sum(TwZ_IlSpr) from cdn.TwrZasoby with(nolock) join cdn.TwrKarty us with(nolock) on Twr_GIDNumer = TwZ_TwrNumer where TwZ_MagNumer = 1 and us.Twr_Kod = ss.twr_kod), 0)- isnull((select sum(Rez_Ilosc) from cdn.Rezerwacje with(nolock) join cdn.TwrKarty us with(nolock) on Twr_GIDNumer = Rez_TwrNumer where Rez_MagNumer = 1 and us.twr_kod = ss.twr_kod and Rez_Aktywna = 1 and Rez_Typ = 1 and Rez_DataWaznosci > DATEDIFF(DD, '18001228', GETDATE())),0) as decimal (10, 0)) end as [quantity],'ean' as [reference_type] from cdn.TwrKarty ss with(nolock) join cdn.Atrybuty with(nolock) on Twr_GIDNumer = Atr_ObiNumer and Atr_OBITyp = 16 and Atr_OBILp = 0 join cdn.AtrybutyKlasy with(nolock) on AtK_ID = Atr_AtkId where Twr_PrdNumer in (19468, 19467) and Atr_Wartosc = 'Standardowy' group by twr_kod,twr_ean";
@@ -31,12 +32,11 @@ namespace StanyMagazynoweFarmitoo
 
             // Pobranie dzisiejszej daty i obecnej godziny a później przekonwertowanie na stringa w formacie RokMiesiącDzieńGodinaMinuta, w celu nazwania pliku
             DateTime dzis = DateTime.Now;
-            string data = dzis.ToString("yyyyMMddHHmm");
+            string data = dzis.ToString("yyyyMMddHHmmss");
 
             // Ścieżka gdzie zapisuje plik .csv
             string filePath = @"C:\temp\stock_" + data + ".csv";
 
-            string ftpUrl = "(adres_serwera_ftp)";
 
             // Konwersja na CSV
             using (StreamWriter writer = new StreamWriter(filePath))
@@ -54,7 +54,7 @@ namespace StanyMagazynoweFarmitoo
 
                 writer.WriteLine();
 
-                // Dane
+                // Dane z zapytania
                 foreach (DataRow row in dataTable.Rows)
                 {
                     for (int i = 0; i < dataTable.Columns.Count; i++)
@@ -71,19 +71,34 @@ namespace StanyMagazynoweFarmitoo
                 }
             }
 
-            
-             
-            using (WebClient client = new WebClient())
-            {
-                // Provide the FTP credentials
-                client.Credentials = new NetworkCredential("login", "hasło");
+            // Dane do logowanaia SFTP
+            string sftpHost = "xxxx";
+            int sftpPort = 22;
+            string sftpUsername = "xxxx";
+            string sftpPassword = "xxxx";
+            string sftpFolderPath = "xxxx";
+            string sftpFileName = "stock_" + data + ".csv";
 
-                // Upload the file
-                client.UploadFile(ftpUrl, "STOR", filePath);
+
+            // Wysyłka danych na sftp
+            using (SftpClient sftpClient = new SftpClient(sftpHost, sftpPort, sftpUsername, sftpPassword))
+            {
+                sftpClient.Connect();
+
+                // Zmiana folderu
+                sftpClient.ChangeDirectory(sftpFolderPath);
+
+                // Wysyłka
+                using (FileStream fileStream = new FileStream(filePath, FileMode.Open))
+                {
+                    sftpClient.UploadFile(fileStream, sftpFileName);
+                }
+
+                sftpClient.Disconnect();
             }
-            
+
             Console.WriteLine("Plik stock_" + data + ".csv został wygenerowany pomyślnie");
-            Console.WriteLine("Plik stock_" + data + ".csv został przesłany na serwer FTP: "+ftpUrl);
+            Console.WriteLine("Plik stock_" + data + ".csv został przesłany na serwer FTP: "+sftpHost);
             Console.ReadKey();
         }
     }
