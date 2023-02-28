e743b885591126f854c9f39af55e870078976447
Krzysztof Kurowski <228528@student.uek.krakow.pl>
2023-02-28 11:15:59 +0100

    Handled cases when there is more than 1 file to download. Now, console shows name of files which you downloaded. Deleted unusing references.

--- PobieranieZamowieniaFarmitoo/PobieranieZamówieniaFarmitoo/Program.cs
+++ PobieranieZamowieniaFarmitoo/PobieranieZamówieniaFarmitoo/Program.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Linq;
-using System.Collections.Generic;
 using Renci.SshNet;
-using Renci.SshNet.Sftp;
 using System.IO;
-using System.Configuration;
-using System.IO.Compression;
+
 
 namespace PobieranieZamówieniaFarmitoo
 {
@@ -20,60 +17,109 @@ namespace PobieranieZamówieniaFarmitoo
             string sftpFolderPath = "xxxx";
             string localPath = @"C:\temp\";
             string sftpArchFolderPath = "xxxx";
-            int j = 0;
 
 
             //Łączenie z serwerem
             using (var client = new SftpClient(sftpHost, sftpPort, sftpUsername, sftpPassword))
             {
                 try
-                {   
+                {
                     client.Connect();
                     Console.WriteLine("Pomyślnie połączono z serwerem " + sftpHost);
 
-                    //Pobieranie plików
+                    //Pobieranie wszystkich plików
                     var files = client.ListDirectory(sftpFolderPath);
+                    int i = files.Count() - 2;
+                    string[] empty = new string[i];
+                    int j = 0;
+
                     foreach (var file in files)
                     {
                         if (!file.Name.StartsWith("."))
                         {
-                            using (Stream fileStream = File.Create(localPath+file.Name))
+                            using (Stream fileStream = File.Create(localPath + file.Name))
                             {
                                 client.DownloadFile(file.FullName, fileStream);
+                                empty[j] = file.Name;
                                 j++;
                             }
                         }
                     }
-
-
-
-                    Console.WriteLine("Pomyślnie pobrano " + j + " plików z katalogu " + sftpFolderPath);
-                    int i = 0;
-                    //Przenoszenie plików do folderu archiwalnego na sftp
-                    if (files != null)
+                    if (i != 0)
                     {
+
+                        if (i == 1)
+                        {
+                            Console.WriteLine("Pomyślnie pobrano " + i + " plik z katalogu " + sftpFolderPath);
+                        }
+                        else if (i == 2 || i == 3 || i == 4)
+                        {
+                            Console.WriteLine("Pomyślnie pobrano " + i + " pliki z katalogu " + sftpFolderPath);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Pomyślnie pobrano " + i + " plików z katalogu " + sftpFolderPath);
+                        }
+
+
+                        //Przenoszenie wszystkich plików do folderu archiwalnego na sftp
                         foreach (var file in files)
                         {
                             if (file.IsDirectory) continue;
                             client.RenameFile(file.FullName, $"{sftpArchFolderPath}/{file.Name}");
-                            i++;
                         }
-                        client.Disconnect();
 
-                        Console.WriteLine("Pomyślnie przeniesiono " + i + " plików do folderu archiwalnego");
+
+                        if (i == 1)
+                        {
+                            Console.WriteLine("Pomyślnie przeniesiono " + i + " plik do folderu archiwalnego");
+                            Console.WriteLine("");
+                            Console.WriteLine("Plik: ");
+                            foreach (var index in empty)
+                            {
+                                Console.WriteLine(index);
+                            }
+                            Console.ReadKey();
+                        }
+                        else if (i == 2 || i == 3 || i == 4)
+                        {
+                            Console.WriteLine("Pomyślnie przeniesiono " + i + " pliki do folderu archiwalnego");
+                            Console.WriteLine("");
+                            Console.WriteLine("Pliki: ");
+                            foreach (var index in empty)
+                            {
+                                Console.WriteLine(index);
+                            }
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Pomyślnie przeniesiono " + i + " plików do folderu archiwalnego");
+                            Console.WriteLine("");
+                            Console.WriteLine("Pliki: ");
+                            foreach (var index in empty)
+                            {
+                                Console.WriteLine(index);
+                            }
+                            Console.ReadKey();
+                        }
+                    
+
                     }
                     else
                     {
-                        Console.WriteLine("Brak plików do pobrania");
-                        client.Disconnect();
+                        Console.WriteLine("Nie ma żadnych plików do pobrania!");
+                        Console.ReadKey();
                     }
+                    client.Disconnect();
                 }
 
-                
 
                 catch (Exception e)
                 {
+                    client.Disconnect();
                     Console.WriteLine("Wystąpił błąd: " + e);
+                    Console.ReadKey();
                 }
             }
         }
